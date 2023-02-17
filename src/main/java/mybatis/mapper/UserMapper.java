1fc78f758885e36f43ea11fcffe736a7394d46e5
周辉生 <391030990@qq.com>
2023-02-17 09:32:16 +0800

    1. 多个散装参数实现多条件模糊查询 2. 利用Map集合封装多个参数实现多条件模糊查询

--- src/main/java/mybatis/mapper/UserMapper.java
+++ src/main/java/mybatis/mapper/UserMapper.java
@@ -1,12 +1,21 @@
 package mybatis.mapper;
 
 import mybatis.pojo.User;
+import org.apache.ibatis.annotations.Param;
 
 import java.util.List;
+import java.util.Map;
 
 public interface UserMapper {
     List<User> selectAll();
     User selectByName(String name);
     User selectById(int id);
     List<User> selectByLessId(int id);
+
+    /**
+     *  多条件查询
+     *      * 必须标注参数在sql中占位符的名字 @Param("占位符名")参数
+     */
+    List<User> selectByCondition(@Param("sex")String sex, @Param("address")String address);
+    List<User> selectByCondition2(Map map);
 }
